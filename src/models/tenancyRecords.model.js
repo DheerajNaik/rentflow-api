f9b9efa947ea51d67118690d5dbd6e6d9d9561de
Dheeraj <dheeraj.naik2000@gmail.com>
2026-07-01 10:49:35 +0530

    feat: get all get,active and single tenancy record

--- src/models/tenancyRecords.model.js
+++ src/models/tenancyRecords.model.js
@@ -6,13 +6,13 @@ const createTenancyRecord = async (record) => {
       const { tenant_id, house_id, move_in_date, number_of_occupants, minimum_stay_months, agreement_done,
         agreement_expiry_date, painting_charges, notes, agreement_file_url } = record;
      const dataWithId = {id,...record}
-   //  console.log(dataWithId)
+ 
         
      const enteredValues =  Object.entries(dataWithId).filter(([k ,v ])=> v !== undefined);
      const values = enteredValues.map(([key,value])=>value);
      const placeholders = enteredValues.map(()=> '?').join(', ');
      const columnNames = enteredValues.map(([key,value])=>key).join(', ');
-    // console.log(columnNames,placeholders,values)
+   
      
     const [[houseOccupied]] = await pool.execute(`SELECT id FROM tenancy_records where house_id = ? AND move_out_date IS NULL`, [house_id]);
     if (houseOccupied) {
@@ -21,9 +21,29 @@ const createTenancyRecord = async (record) => {
     const [[tenantAlreadyLivingElsewhere]] = await pool.execute(`SELECT id FROM tenancy_records where tenant_id = ? AND move_out_date IS NULL`, [tenant_id]);
 
     const result = await pool.execute(`INSERT INTO tenancy_records (${columnNames} ) VALUES (${placeholders})`,values)
-    console.log(result)
+   
   return { result, warning: tenantAlreadyLivingElsewhere ? "Tenant is also staying in another house" : null };
 
 }
 
-module.exports = {createTenancyRecord}
+const getAllTenancyRecords = async()=>{
+      const [results] =  await pool.execute(`SELECT * FROM tenancy_records`);
+      
+      return results;
+}
+
+const getAllActiveTenancyRecords = async ()=>{
+       const [results] = await pool.execute(`SELECT * FROM tenancy_records WHERE move_out_date is NULL`)
+       console.log(results)
+       return results;
+}
+const getTenancyRecordById = async (id) =>{
+       const [[result]] =   await pool.execute((`SELECT * FROM tenancy_records WHERE id = ? `), [id]);
+       if(!result){
+        return "Invalid Request"
+       }
+       return result;
+
+}
+
+module.exports = {createTenancyRecord, getAllTenancyRecords, getAllActiveTenancyRecords, getTenancyRecordById}
